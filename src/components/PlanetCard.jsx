93205e0a31260bdfc6ed3d67684c65da5a2e0e71
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-06-21 16:32:26 -0300

    Requisito 5 :roquet:

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropsTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planetName">{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropsTypes.string,
+  planetImage: PropsTypes.string,
+}.isRequierd;
+
+export default PlanetCard;
